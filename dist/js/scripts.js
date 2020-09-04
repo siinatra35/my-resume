404c85ade3e147d0ec1894099f18e1d2bb330b6f
david <dacuna305@gmail.com>
2020-09-03 23:00:05 -0500

    added new color theme and new icon background

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -9,7 +9,7 @@
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
-            location.pathname.replace(/^\//, "") ==
+            location.pathname.replace(/^\//, "") ===
                 this.pathname.replace(/^\//, "") &&
             location.hostname == this.hostname
         ) {
@@ -47,4 +47,6 @@ const chk = document.getElementById('chk');
 chk.addEventListener('change', () => {
     document.body.classList.toggle('dark');
 });
+
+
 
